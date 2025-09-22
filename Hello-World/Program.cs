71e6c7ec5d26bc1eb46b1590743e8ea5bf58cd3c
Nuno Reis <nuno.reis@namecheap.com>
2025-09-22 12:07:33 +0100

    chore: fix issues

--- Hello-World/Program.cs
+++ Hello-World/Program.cs
@@ -27,23 +27,12 @@ static string SignData(string data)
     var keyXml = System.IO.File.ReadAllText("private_key.xml"); // Replace with your key file path
     privateKey.FromXmlString(keyXml);
 
-    int a = 0;
-    int b = 1 / a; // This will throw DivideByZeroException
-
-    if (a == b)
-    {
-        throw new Exception("Simulated error after loading private key.");
-    }
-    else
-    {
-        Console.WriteLine("No error occurred.");
-    }
-
     var bytesToSign = Encoding.UTF8.GetBytes(data);
     var signatureBytes = privateKey.SignData(bytesToSign, HashAlgorithmName.SHA256, RSASignaturePadding.Pkcs1);
-    return Convert.ToBase64String(signatureBytes);
 
-    Console.WriteLine("Nonsense write after return");
+    privateKey.Dispose();
+
+    return Convert.ToBase64String(signatureBytes);
 }
 
 bool VerifySignature(string data, string signatureBase64)
